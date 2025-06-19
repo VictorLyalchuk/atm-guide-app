2b94b64e4531cf228728df69bc596324e14b5bc4
BloodyRoom <158325416+BloodyRoom@users.noreply.github.com>
2025-06-19 13:17:01 +0300

    Revert "Робота Android/Ios додаток"

--- App.tsx
+++ App.tsx
@@ -1,35 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useEffect, useState } from 'react';
-import { getToken, refreshToken } from "./src/services/account-services";
 
 export default function App() {
-  const [ msg, setMsg ] = useState("");
-
-  useEffect(() => {
-    const checkToken = async () => {
-      const token = await getToken();
-      if (token) {
-        try {
-          await refreshToken();
-          // код коли сесія продовжилась
-          setMsg("Session continue");
-        } catch (error) {
-          // код коли сесія завержується
-          setMsg("Session closed");
-        }
-      } else {
-        // код якщо токену нема
-        setMsg("Token not found");
-      }
-    };
-
-    checkToken();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>{msg}</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
